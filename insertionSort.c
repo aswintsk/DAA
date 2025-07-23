ef4ed184b4e658d65d050724464af4f9ba7cfe86
aswintsk <aswinsunil2402@gmail.com>
2025-07-23 08:43:23 +0530

    Update insertionSort.c

--- insertionSort.c
+++ insertionSort.c
@@ -1,2 +1,41 @@
-#include<stdio.h>
+#include <stdio.h>
 
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+
+    for (i = 1; i < n; i++) {
+        key = arr[i];      // element to be inserted
+        j = i - 1;
+
+        // Shift elements greater than key to one position ahead
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        // Insert key into correct position
+        arr[j + 1] = key;
+    }
+}
+
+// Utility function to print the array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    insertionSort(arr, n);
+
+    printf("Sorted array:   ");
+    printArray(arr, n);
+
+    return 0;
+}
